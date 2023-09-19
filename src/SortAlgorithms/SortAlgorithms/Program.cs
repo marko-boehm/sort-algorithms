3d7210286baa2219b8f01b3c1a65ae3a4d4274c4
Marko Boehm <boehm.marko@outlook.com>
2023-09-19 21:05:15 +0200

    Add binary tree

--- src/SortAlgorithms/SortAlgorithms/Program.cs
+++ src/SortAlgorithms/SortAlgorithms/Program.cs
@@ -2,24 +2,31 @@
 using SortAlgorithms;
 using SortAlgorithms.DivideAndConquerSortings;
 using SortAlgorithms.SimpleSortings;
+using SortAlgorithms.TreeSortings;
 
-
-var randomNumbers = Enumerable.Range(1, 10000).OrderBy(n => Guid.NewGuid()).ToArray(); //100000
+var randomNumbers = Enumerable.Range(1, 100000).OrderBy(n => Guid.NewGuid()).ToArray(); //100000
 
 Console.WriteLine("Comparison of sort algorithms");
 Console.WriteLine("-----------------------------");
 Console.WriteLine($"Count of random numbers: {randomNumbers.Count()}\n");
 
 //randomNumbers = new int[] { 2, 7, 3, 5, 4, 9, 10, 6, 8, 1 };
-//var sort = new QuickSorting();
+//var binSearchTree = new BinarySearchTree();
+//binSearchTree.AddData(randomNumbers);
+//var arr = binSearchTree.InOrder();
+
+//randomNumbers = Enumerable.Range(1, 100).OrderBy(n => Guid.NewGuid()).ToArray();
+//var sort = new BubbleSortingImproved();
 //var sortedList = sort.Sort(randomNumbers);
 
 List<ISort> sortAlgorithms = new List<ISort>();
 sortAlgorithms.Add(new SelectionSorting());
 sortAlgorithms.Add(new InsertionSorting());
 sortAlgorithms.Add(new BubbleSorting());
+sortAlgorithms.Add(new BubbleSortingV2());
 sortAlgorithms.Add(new MergeSorting());
 sortAlgorithms.Add(new QuickSorting());
+sortAlgorithms.Add(new BinaryTreeSorting());
 sortAlgorithms.Add(new ArraySortingCSharp());
 
 foreach (ISort sortAlgorithm in sortAlgorithms)
---
+++ src/SortAlgorithms/SortAlgorithms/SimpleSortings/BubbleSortingV2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAlgorithms.SimpleSortings
+{
+    /// <summary>
+    /// Wiki: https://en.wikipedia.org/wiki/Bubble_sort
+    /// </summary>
+    public class BubbleSortingV2 : ISort
+    {
+        public int[] Sort(int[] arrayToSort)
+        {
+            int[] sortedArray = (int[])arrayToSort.Clone();
+            int index = sortedArray.Length - 1;
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int latestSwappedIndex = sortedArray.Length - 1;
+
+            // Algorithm for Bubble Sort
+            while (index > 0)
+            {
+                for (int i = 0; i < index; i++)
+                {
+                    if (sortedArray[i] > sortedArray[i + 1])
+                    {
+                        Swap(sortedArray, i, i + 1);
+                        latestSwappedIndex = i;
+                    }
+                }
+
+                if (index > latestSwappedIndex)
+                {
+                    index = latestSwappedIndex;
+                }
+                else
+                {
+                    index--;
+                }
+                
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine("Bubble Sort V2 elapsed time(ms) / ticks: {0,10} \t {1,15}", stopwatch.ElapsedMilliseconds, stopwatch.ElapsedTicks);
+            return sortedArray;
+        }
+
+
+        private void Swap(int[] sortedList, int i, int j)
+        {
+            int tmp = sortedList[i];
+            sortedList[i] = sortedList[j];
+            sortedList[j] = tmp;
+        }
+    }
+}
